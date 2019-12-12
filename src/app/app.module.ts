91d6204be815e67c5b61c53dbfcc1843687295bf
Pandian <765707@cognizant.com>
2019-12-12 12:01:52 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'dashboard', component: DashboardComponent},
+      {path:'login', component: LoginComponent},
+      {path: '', redirectTo: 'login', pathMatch:'full'},
+      {path: '**', component: LoginComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
