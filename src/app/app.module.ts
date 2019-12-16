4da2182d68eb16abb66e99390524d5f1bc6492a0
vignesh20794 <761783@AMMIN00809.local>
2019-12-16 16:38:36 +0530

    header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { LoginComponent } from './login/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule , MatCardModule} from '@angular/material';
+import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule , MatCardModule ,MatToolbarModule} from '@angular/material';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -26,6 +26,7 @@ import { FormsModule } from '@angular/forms';
     MatCardModule,
     MatIconModule,
     MatSelectModule,
+    MatToolbarModule,
     RouterModule.forRoot([
       {path: 'dashboard', component: DashboardComponent},
       {path:'login', component: LoginComponent},
